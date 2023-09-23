106989153b01764ec7e0a11afb7fe8019a481c98
Jack Rabe <jhrabe@wisc.edu>
2023-09-23 14:11:17 -0500

    fix input for question response

--- backend/question.go
+++ backend/question.go
@@ -17,7 +17,7 @@ type QuestionResponse = struct {
 func getQuestion(w http.ResponseWriter, r *http.Request) {
 	question := QuestionResponse{
 		Question:    "what is the smallest element in this array",
-		Inputs:      []string{"[1], [1,2]"},
+		Inputs:      []string{"[1]", "[1,2]"},
 		Solution:    "this is a solution",
 		SampleInput: "[3,2,1]",
 		Explanation: "this is an explanation"}
