5a03b1e7ccc8165099e67c064b0a2e8a465fc0b4
Jack Rabe <jhrabe@wisc.edu>
2023-09-23 21:23:38 -0500

    add db for answer checking

--- backend/answer.go
+++ backend/answer.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type PostAnswerRequest = struct {
@@ -15,6 +18,11 @@ type PostAnswerRequest = struct {
 type PostAnswerResponse = struct {
 	HasWon          bool  `json:"hasWon"`
 	MissedQuestions []int `json:"missedQuestions"`
+}
+
+type X = struct {
+	Day      int      `json:"day"`
+	Expected []string `json:"expected"`
 }
 
 func postAnswer(w http.ResponseWriter, r *http.Request) {
@@ -32,11 +40,34 @@ func postAnswer(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	answerReponse := PostAnswerResponse{HasWon: false, MissedQuestions: []int{1, 2}}
+	db, client := connectToMongo()
+	collection := db.Collection("answers")
+	answerObject := collection.FindOne(context.Background(), bson.M{})
+	var answerResult X
+	err = answerObject.Decode(&answerResult)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(answerResult)
+	missedQuestions := make([]int, 0)
+	for idx, expected := range answerResult.Expected {
+		fmt.Println(req.Responses[idx], expected)
+		if expected != req.Responses[idx] {
+			missedQuestions = append(missedQuestions, idx)
+		}
+
+	}
+	answerReponse := PostAnswerResponse{HasWon: false, MissedQuestions: missedQuestions}
 	responseBody, err := json.Marshal(answerReponse)
 	if err != nil {
 		panic(err)
 	}
+
+	defer func() {
+		if err = client.Disconnect(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
 
 	io.WriteString(w, string(responseBody))
 }
